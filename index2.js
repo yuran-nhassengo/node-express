b9deebd9b64bf2a0197ec6f8818581ad88e3052d
yuran-nhassengo <yurannhassengo@gmail.com>
2024-06-08 02:42:00 +0200

    Using params express

--- index2.js
+++ index2.js
@@ -15,8 +15,8 @@ app.get('/blog',(req,res) =>{
         res.send('<h1>Bem-vindo ao meu Blog</h1>');
     });
 
-app.get('/ola/:nome',(req,res) =>{
-    res.send('Ola');
+app.get('/ola/:cargo/:nome',(req,res) =>{
+    res.send('<h1>Ola '+req.params.nome+'</h1>'+'<h2>Seu Cargo: '+req.params.cargo+'</h2>');
 });
 
 app.listen(3000,()=>{
