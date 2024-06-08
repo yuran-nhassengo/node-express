f25ce3d99970c24a20595e12078bea28f424108f
yuran-nhassengo <yurannhassengo@gmail.com>
2024-06-08 02:45:21 +0200

    Install nodemon

--- index2.js
+++ index2.js
@@ -3,17 +3,6 @@ const express = require('express');
 const app = express();
 
 
-app.get('/',(req,res) =>{
-    res.send('<h1>Seja Bem-vindo ao meu App');
-});
-
-app.get('/sobre',(req,res) =>{
-    res.send('<h1>Minha pagina Sobre');
-});
-
-app.get('/blog',(req,res) =>{
-        res.send('<h1>Bem-vindo ao meu Blog</h1>');
-    });
 
 app.get('/ola/:cargo/:nome',(req,res) =>{
     res.send('<h1>Ola '+req.params.nome+'</h1>'+'<h2>Seu Cargo: '+req.params.cargo+'</h2>');
